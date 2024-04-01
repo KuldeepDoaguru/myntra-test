31928a62ad835c30a382b93958b071602d757e2e
KuldeepDoaguru <kuldeepdoauruinfosystems@gmail.com>
2024-04-01 11:08:27 +0530

    cart items section updated
    cart items section updated

--- frontend/src/components/CartItems.jsx
+++ frontend/src/components/CartItems.jsx
@@ -52,7 +52,7 @@ const CartItems = () => {
             return null;
           })}
           <div className="cartitems-down">
-            <div className="carftitems-total">
+            <div className="cartitems-total">
               <h1>Cart Total</h1>
               <div>
                 <div className="cartitems-total-item"></div>
@@ -69,7 +69,14 @@ const CartItems = () => {
                 <h3>Total</h3>
                 <h3>${0}</h3>
               </div>
-              <hr />
+              <button>PROCEED TO CHECKOUT</button>
+            </div>
+            <div className="cartitems-promocode">
+              <p>If you have a promo code, enter it here</p>
+              <div className="cartitems-promobox">
+                <input type="text" placeholder="promo code" />
+                <button>Submit</button>
+              </div>
             </div>
           </div>
         </div>
@@ -119,5 +126,70 @@ const Container = styled.div`
     height: 50px;
     border: 2px solid #ebebeb;
     background: #fff;
+  }
+
+  .cartitems-down {
+    display: flex;
+    margin: 100px 0px;
+  }
+
+  .cartitems-total {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    margin-right: 200px;
+    gap: 40px;
+    button {
+      width: 262px;
+      height: 58px;
+      outline: none;
+      border: none;
+      background: #ff5a5a;
+      color: #fff;
+      font-size: 16px;
+      font-weight: 600;
+      cursor: pointer;
+      border-radius: 5px;
+    }
+  }
+
+  .cartitems-total-items {
+    display: flex;
+    justify-content: space-between;
+    padding: 15px 0px;
+  }
+
+  .cartitems-promocode {
+    flex: 1;
+    font-size: 16px;
+    font-weight: 500;
+    p {
+      color: #555;
+    }
+  }
+
+  .cartitems-promobox {
+    width: 504px;
+    margin-top: 15px;
+    padding-left: 20px;
+    height: 58px;
+    background: #eaeaea;
+    display: flex;
+    input {
+      border: none;
+      outline: none;
+      background: transparent;
+      font-size: 16px;
+      width: 330px;
+      height: 50px;
+    }
+    button {
+      width: 170px;
+      height: 58px;
+      font-size: 16px;
+      background: black;
+      color: white;
+      cursor: pointer;
+    }
   }
 `;
